8e7c71c711827797cfa2248f44a93b539eac6aa9
gabrieldevreis <gabrielreisdev@gmail.com>
2026-02-16 17:20:12 -0300

    feat: integrate UserService into AppComponent to fetch and log user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CountriesService } from './services/countries.service';
 import { StatesService } from './services/states.service';
 import { CitiesService } from './services/cities.service';
+import { UserService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,8 @@ export class AppComponent implements OnInit {
   constructor(
     private readonly _countriesService: CountriesService,
     private readonly _statesService: StatesService,
-    private readonly _citiesService: CitiesService
+    private readonly _citiesService: CitiesService,
+    private readonly _userService: UserService
 
   ){}
 
@@ -27,6 +29,10 @@ export class AppComponent implements OnInit {
 
     this._citiesService.getCities('Brazil','São Paulo').subscribe((citiesResponse: any)=>{
       console.log('citiesResponse', citiesResponse)
+    })
+
+    this._userService.getUsers().subscribe((usersResponse)=>{
+      console.log('userResponse', usersResponse)
     })
   }
 }
